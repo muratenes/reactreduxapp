d9456ca2e5bf6826a06e5aeaa1ab7e97ec742c15
muratenes <m9urat@gmail.com>
2020-07-04 11:56:54 +0300

    redux dev tools kuruldu

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers/rootReducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+import {Provider} from 'react-redux';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
